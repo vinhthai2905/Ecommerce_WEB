99d8459f82be6a297417aa6b6ca44103148d3ada
DESKTOP-V88JK8B\hi <vinhthai2905@aol.com>
2024-11-27 16:49:09 +0700

    done migrations and models

---
+++ database/migrations/2024_11_27_093932_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100)->unique();
+            $table->string('image', 100);
+            $table->float('price', 10.2);
+            $table->text('content');
+            $table->unsignedInteger('category_id');
+            $table->tinyInteger('status')->default(0);
+            $table->timestamps();
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
