75bfa27f5d533a03df6a200ab6e6d14442a1c326
DESKTOP-V88JK8B\hi <vinhthai2905@aol.com>
2024-11-30 00:34:34 +0700

    done model product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'image',
+        'price',
+        'sale_price',
+        'slug',
+        'category_id',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($product) {
+            $product->slug = $product->slug ? Str::slug($product->slug) : Str::slug($product->name);
+        });
+    }
 }
