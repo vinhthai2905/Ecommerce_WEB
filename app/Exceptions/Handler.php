babdde144bb2c2956654fb3ba202b6a3cf5f96d8
vinhthai2905-admin <vinhthai2905@aol.com>
2024-11-30 17:14:46 +0700

    adding handling exceptions for unauthenticated user, render view

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,47 +2,48 @@
 
 namespace App\Exceptions;
 
+use App\Traits\HttpResponses;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Throwable;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of exception types with their corresponding custom log levels.
-     *
-     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
-     */
-    protected $levels = [
-        //
-    ];
 
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array<int, class-string<\Throwable>>
-     */
-    protected $dontReport = [
-        //
-    ];
+    use HttpResponses;
 
-    /**
-     * A list of the inputs that are never flashed to the session on validation exceptions.
-     *
-     * @var array<int, string>
-     */
     protected $dontFlash = [
         'current_password',
         'password',
         'password_confirmation',
     ];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     */
+    public function render($request, Throwable $e) : Response
+    {
+        if ($e instanceof ValidationException) {
+            return $this->errorResponse(
+                $e->getMessage(),
+                Response::HTTP_UNPROCESSABLE_ENTITY,
+                $e->errors()
+            );
+        }
+        return parent::render($request, $e);
+    }
+
     public function register(): void
     {
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception): Response
+    {
+        return $request->expectsJson()
+            ? $this->errorResponse($exception->getMessage(), 401)
+            : $this->errorResponse('You are not allowed to access', 401);
     }
 }
