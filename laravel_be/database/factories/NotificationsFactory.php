2f1a6ccfb1f28753a6e95937571a9be798a1a082
vinhthai2905-admin <vinhthai2905@aol.com>
2024-12-03 22:31:03 +0700

    seeder for creating fake datas for testing purpose

--- laravel_be/database/factories/NotificationsFactory.php
+++ laravel_be/database/factories/NotificationsFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Notification>
  */
-class NotificationFactory extends Factory
+class NotificationsFactory extends Factory
 {
     /**
      * Define the model's default state.
--- laravel_be/database/seeders/DatabaseSeeder.php
+++ laravel_be/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,13 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Categories;
+use App\Models\Product;
+use App\Models\ImageProducts;
+use App\Models\Carts;
+use App\Models\Bills;
+use App\Models\Notifications;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +19,47 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // Create 5 users
+        $users = User::factory(5)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Create 5 categories
+        $categories = Categories::factory(5)->create();
+
+        // Create 5 products for each category
+        $categories->each(function ($category) {
+            $products = Product::factory(5)->create(['category_id' => $category->id]);
+
+            // Create 5 images for each product
+            $products->each(function ($product) {
+                ImageProducts::factory(5)->create(['product_id' => $product->id]);
+            });
+        });
+
+        // Create 5 carts for each user
+        $users->each(function ($user) {
+            $products = Product::inRandomOrder()->take(5)->get();
+            $products->each(function ($product) use ($user) {
+                Carts::factory()->create([
+                    'user_id' => $user->id,
+                    'product_id' => $product->id,
+                ]);
+            });
+        });
+
+        // Create 5 bills for each user
+        $users->each(function ($user) {
+            $products = Product::inRandomOrder()->take(5)->get();
+            $products->each(function ($product) use ($user) {
+                Bills::factory()->create([
+                    'user_id' => $user->id,
+                    'product_id' => $product->id,
+                ]);
+            });
+        });
+
+        // Create 5 notifications for each user
+        $users->each(function ($user) {
+            Notifications::factory(5)->create(['user_id' => $user->id]);
+        });
     }
 }
